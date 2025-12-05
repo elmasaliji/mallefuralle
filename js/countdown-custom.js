727f1f9454e82bef2de4e5cd05e93071e4479c6c
Elma <117161570+elmasaliji@users.noreply.github.com>
2025-12-05 08:38:35 +0100

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2025, 12, 1, 12)}); // year, month, date, hour
+        });
+});		
+
